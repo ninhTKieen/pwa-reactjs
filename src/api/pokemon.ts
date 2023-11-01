ca5243e6f080e3836c95bde293a0869d6d7312ef
ninhTKieen <ninhtrungkien2811@gmail.com>
2023-11-01 14:31:03 +0700

    chore: change code

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = `https://pokeapi.co/api/v2/pokemon?limit=156`;
+const URL = `https://pokeapi.co/api/v2/pokemon?limit=159`;
 
 export const getAllPokemonList = async () => {
   const { data } = await axios.get(URL);
